19a261944235ea857bf65db2f0bcc28cb5dea310
xeliot <daveho8888@gmail.com>
2018-06-13 11:07:43 -0700

    clean up data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -16,21 +16,15 @@ def hello():
 
 @application.route("/csv", methods=['POST'])
 def getCSV():
-    print("CSVVVVVV")
     data = json.loads(request.data)
-    print(type(data))
-    print(data["responseContent"])
     #rint(request.data)
     responseContent = data["responseContent"]
-    print("hERE")
-    print(list(responseContent[0].keys()))
     f = csv.writer(open("data.csv", "w"))
     f.writerow(list(responseContent[0].keys()))
     for response in responseContent:
         row = []
         for key in responseContent[0].keys():
             row.append(response[key])
-        print(row)
         f.writerow(row)
     #s3.upload_file(filename, bucket_name, filename)
 
