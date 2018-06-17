ea7f11c6a3e1d13fb951bc51f519281d7f185a21
xeliot <daveho8888@gmail.com>
2018-06-17 15:06:20 -0700

    start using sentiment analysis with TextBlob

--- data_analysis.py
+++ data_analysis.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 import collections
+from textblob import TextBlob
 
 
 application = Flask(__name__)
@@ -57,7 +58,7 @@ def oneVarNum():
 
 @application.route("/oneVarMC", methods=['POST'])
 def oneVarMC():
-    srcData = {}
+    srcList = []
     data = json.loads(request.data)
     print(data)
     fig = plt.figure()
@@ -66,21 +67,60 @@ def oneVarMC():
     imgdata = BytesIO()
     fig.savefig(imgdata, format='png')
     imgdata.seek(0)
+    src = base64.encodebytes(imgdata.getvalue()).decode()
+    print(src)
+    srcList.append(src)
+    srcData = {"srcList": srcList}
+    return jsonify(srcData)
 
-    print (base64.encodebytes(imgdata.getvalue()).decode())
-    print ("Content-type: image/png\n")
-    uri = 'data:image/png;base64,' + base64.encodebytes(imgdata.getvalue()).decode()
-    print ('<img src = "%s"/>' % uri)
-    return "success"
-
-
-    #piechart below
-    """ {‘first’: [‘Brazil’, ‘Brazil’, ‘Brazil’, ‘Portugal’, ‘Germany’, ‘Germany’, ‘Argentina’, ‘Argentina’, ‘Brazil’, ‘Germany’], ‘firstQuestionField’: ‘Who do you think will win the World Cup?’}"""
-    responseFrequencyComplete = collections.Counter(data['first'])
+    '''
+    #pandas piechart below
+    hardcodedData = {'first': ['Brazil', 'Brazil', 'Brazil', 'Portugal', 'Germany', 'Germany', 'Argentina', 'Argentina', 'Brazil', 'Germany'], 'firstQuestionField': 'Who do you think will win the World Cup?'}
+    responseFrequencyComplete = collections.Counter(hardcodedData['first'])
     responseFrequencyKeys = responseFrequencyComplete.keys()
     responseFrequencyValues = responseFrequencyComplete.values()
 
-    pieChartDF = df = pd.DataFrame({'mass': responseFrequencyValues}, index = responseFrequencyKeys)
+    pieChartDF = pd.DataFrame({'vals': responseFrequencyValues}, index = responseFrequencyKeys)
+    pieChartVisual = pieChartDF.plot.pie(y='vals', figsize=(5, 5))
+    #^^^^^^ that should be the image if the code works
+    print(type(pieChartVisual))
+
+    #matplotlib piechart
+    hardcodedData = {'first': ['Brazil', 'Brazil', 'Brazil', 'Portugal', 'Germany', 'Germany', 'Argentina', 'Argentina', 'Brazil', 'Germany'], 'firstQuestionField': 'Who do you think will win the World Cup?'}
+    responseFrequencyComplete = collections.Counter(hardcodedData['first'])
+    responseFrequencyKeys = responseFrequencyComplete.keys()
+    responseFrequencyValues = responseFrequencyComplete.values()
+
+    pieChartDF = pd.DataFrame(, index = responseFrequencyKeys)
+    '''
+
+@application.route("/oneVarLongText", methods=['POST'])
+def oneVarLongText():
+    data = json.loads(request.data)
+    srcList = []
+    sentiment_analysis = {
+        "very_positive": 0,
+        "somewhat_positive": 0,
+        "neutral": 0,
+        "somewhat_negative": 0,
+        "very_negative": 0
+    }
+    print(data)
+    for response in data["first"]:
+        polarity = TextBlob(response).sentiment.polarity
+        if(polarity < -0.6):
+            sentiment_analysis["very_negative"] += 1
+        elif(polarity < -0.2):
+            sentiment_analysis["somewhat_negative"] += 1
+        elif(polarity < 0.2):
+            sentiment_analysis["neutral"] += 1
+        elif(polarity < 0.6):
+            sentiment_analysis["somewhat_positive"] += 1
+        else:
+            sentiment_analysis["very_positive"] += 1
+    print(sentiment_analysis)
+    return("oneVarLongText success")
+
 
 
 if __name__ == "__main__":
